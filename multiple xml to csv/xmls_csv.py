234df5d7127a81459bae603e36b43ff265963329
Adriane Hanson <ahansonuga@gmail.com>
2019-06-06 16:25:49 -0400

    Add files via upload
    Upload of first content for this repo.

---
+++ multiple xml to csv/xmls_csv.py
@@ -0,0 +1,159 @@
+# Purpose: copy data from multiple xml files into a single csv, 1 row per xml file, for data analysis.
+
+"""
+Before doing transformations on a batch of xml, we want to know if there are complications in the data to address. This can be quickly done by opening the csv in a spreadsheet and sorting and/or filtering each column. Look for things like empty fields, abbreviations to spell out, and different ways that dates or other data are formatted.
+
+The xml this script is designed for had all elements as direct children of the root. All elements could be optional and could be repeated.
+    
+Before running the script, put all xml files to be included in the csv into a folder. Enter the path for that folder in line 10.
+"""
+
+import os
+import xml.etree.ElementTree as ET
+import csv
+
+# change to directory containing xml input files
+os.chdir('INSERT PATH TO FOLDER WITH XML HERE')
+
+# make csv file to save data to
+csv_file = open('../combined-xml.csv', 'w', newline = '', encoding = 'UTF8')
+csvwriter = csv.writer(csv_file)
+
+# add header row to the csv file
+csvwriter.writerow(['title', 'identifier', 'source', 'coverage', 'date', 'publisher', 'description', 'subject', 'altid'])
+
+# iterate over each xml file and create a row in the csv file
+for file in os.listdir('.'):
+    
+    # read the xml file
+    tree = ET.parse(file)
+    root = tree.getroot()
+
+    # store the values of each element of interest as variables to be written to a csv row later.
+    for dc in root.findall('.'):
+
+        # get the value(s) from the title element(s), which may appear 0, 1, or multiple times.
+        # this block of code (line 39-54) is then repeated for every element to be included in the csv.
+        title = dc.findtext('title')
+        # if there is no title element, put a value of "N/A" in the csv.
+        if title is None:
+            title = "N/A"
+        else:
+            # find all instances of title, temporarily save to a list, and then combine all titles from the list into a single string with a | between each title.
+            title_list = []
+            for title in dc.findall('title'):
+                title_list.append(title.text)
+                # if a title element is present but empty, it will return None which will give an error when try to join.
+                # remove None from the list and display a message in the terminal so we know there are blanks.
+                if None in title_list:
+                    title_list.remove(None)
+                    print("title has empty tags")
+                # this is the value that is going to be in the csv.
+                title = ' | '.join(title_list)
+
+        id = dc.findtext('identifier')
+        if id is None:
+            id = "N/A"
+        else:
+            id_list = []      
+            for id in dc.findall('identifier'):
+                id_list.append(id.text)
+                if None in id_list:
+                    id_list.remove(None)
+                    print("identifier has empty tags")
+                id = ' | '.join(id_list)
+ 
+
+        source = dc.findtext('source')
+        if source is None:
+            source = "N/A"
+        else:
+            source_list = []
+            for source in dc.findall('source'):
+                source_list.append(source.text)
+                if None in source_list:
+                    source_list.remove(None)
+                    print("source has empty tags")
+                source = ' | '.join(source_list)
+
+        cov = dc.findtext('coverage')
+        if cov is None:
+            cov = "N/A"
+        else:
+            cov_list = []
+            for cov in dc.findall('coverage'):
+                cov_list.append(cov.text)
+                if None in cov_list:
+                    cov_list.remove(None)
+                    print("coverage has empty tags")
+                cov = ' | '.join(cov_list)
+
+        date = dc.findtext('date')
+        if date is None:
+            date = "N/A"
+        else:
+            date_list = []
+            for date in dc.findall('date'):
+                date_list.append(date.text)
+                if None in date_list:
+                    date_list.remove(None)
+                    print("date has empty tags")
+                date = ' | '.join(date_list)
+
+        pub = dc.findtext('publisher')
+        if pub is None:
+            pub = "N/A"
+        else:
+            pub_list = []
+            for pub in dc.findall('publisher'):
+                pub_list.append(pub.text)
+                if None in pub_list:
+                    pub_list.remove(None)
+                    print("publisher has empty tags")
+                pub = ' | '.join(pub_list)
+
+        desc = dc.findtext('description')
+        if desc is None:
+            desc = "N/A"
+        else:
+            desc_list = []
+            for desc in dc.findall('description'):
+                desc_list.append(desc.text)
+                if None in desc_list:
+                    desc_list.remove(None)
+                    print("description has empty tags")
+                desc = ' | '.join(desc_list)
+
+       
+        sub = dc.findtext('subject')
+        if sub is None:
+            sub = "N/A"
+        else:
+            sub_list = []
+            for sub in dc.findall('subject'):
+                sub_list.append(sub.text)
+                if None in sub_list:
+                    sub_list.remove(None)
+                    print("subject has empty tags")
+                sub = ' | '.join(sub_list)
+
+        altid = dc.findtext('identifier.alternate')
+        if altid is None:
+            altid = "N/A"
+        else:
+            altid_list = []
+            for altid in dc.findall('identifier.alternate'):
+                altid_list.append(altid.text)
+                if None in altid_list:
+                    altid_list.remove(None)
+                    print("identifier.alternate has empty tags")
+                altid = ' | '.join(altid_list)                
+                
+
+    #Write all the values for the xml file to a row in the csv file
+    csvwriter.writerow([title, id, source, cov, date, pub, desc, sub, altid])
+
+# close csv file
+csv_file.close
+
+print("Done!")
---
+++ multiple xml to one ead/xmls-to-ead.py
@@ -0,0 +1,115 @@
+# Purpose: take data from multiple xml files and combine them into a single dsc section of an EAD finding aid.
+
+"""
+This script was used to create a single finding aid for a group of items that were described individually in a database. The database exported one xml file per item.
+
+The xml this script is designed for had all elements as direct children of the root. Some elements were optional and some could be repeated.
+    
+Before running the script, put all xml files to be included in the EAD into a folder. Enter the path for that folder in line 17.
+
+After running the script, paste everything into an xml file that has the collection-level elements needed to be able to import it into Archivists' Toolkit for further editing.
+"""
+
+import xml.etree.ElementTree as ET
+import os
+import re
+
+xml_directory = 'INSERT PATH TO FOLDER WITH XML HERE'
+
+# make a new xml document, with a root element named dsc.
+combo = ET.ElementTree(ET.Element('dsc'))
+combo_root = combo.getroot()
+
+# iterate over each xml file and make the c01 element and all its children.
+for xml in os.listdir(xml_directory):
+
+    # read the xml file.
+    ET.register_namespace('xtf',"http://cdlib.org/xtf")
+    tree = ET.parse(f'{xml_directory}/{xml}')
+    root = tree.getroot()
+    
+    # get values from xml and store in variables, transforming if needed (lines 31-90).
+    # each variable has the value for one EAD element.
+    title = ''
+    for title in root.findall('title'):
+        title = title.text
+        
+    identifier = ''
+    for id in root.findall('identifier'):
+        identifier = id.text
+        
+    publisher = ''
+    for pub in root.findall('publisher'):
+        publisher = pub.text
+        
+    description = ''
+    for desc in root.findall('description'):
+        description = desc.text
+    
+    # combine publisher and description to use for the scope and content note.
+    # an xml file may have both, only one, or neither of these two elements.
+    scope = ''
+    if publisher == '':
+        # if there isn't publisher or description.
+        if description == '':
+            scope = 'empty'
+        # if there is only description.
+        else:
+            scope = description
+    else:
+        # if there is only publisher.
+        if description == '':
+            scope = f'Publisher: {publisher}'
+        # if there is publisher and description.
+        else:
+            scope = f'Publisher: {publisher}; {description}'
+    
+    # format the date information.
+    # get start and end year to use for the normal attribute in EAD.
+    # get coverage (a separate date element in the xml) which gives information on if the date is approximate or not.
+    coverage = ''
+    for coverage in root.findall('coverage'):
+        coverage = coverage.text
+    
+    date = ''
+    start_year = ''
+    end_year = ''
+    for date in root.findall('date'):
+        # the date is a year range, which is formatted YYYY-YYYY
+        if '-' in date.text:
+            regex = re.match(r'(\d{4})-(\d{4})', date.text)
+            start_year = regex.group(1)
+            end_year = regex.group(2)
+        # the date is a single year, which is formatted YYYY
+        else:
+            start_year = date.text
+            end_year = date.text
+
+        # if any of these three characters are in coverage, the date is approximate.
+        if 's' in coverage or '[' in coverage or 'ca.' in coverage:
+            date = f'circa {date.text}'
+        else:
+            date = date.text
+
+    # make the EAD, which is a c01 element and all its children.
+    
+    root = ET.Element('c01', level="item")
+    did = ET.SubElement(root, 'did')
+    ET.SubElement(did, 'unittitle').text = title
+    ET.SubElement(did, 'container', type="Item").text = identifier
+    
+    if date == 'n.d.' or date == 'n.d,':
+        ET.SubElement(did, 'unitdate').text = 'undated'
+    else:
+        ET.SubElement(did, 'unitdate', normal=f'{start_year}/{end_year}').text = date
+    
+    if scope != 'empty':
+        sc = ET.SubElement(root, 'scopecontent')
+        ET.SubElement(sc, 'head').text = 'Scope and Contents note'
+        ET.SubElement(sc, 'p').text = scope
+    
+    # add the c01 for this xml file to the dsc element.
+    combo_root.append(root)
+
+# save all the c01s to the xml file.    
+combo.write('H:/python/hargrett/broadsides/dsc.xml')
